ec322828b58fa1c9a2a309fbc10be51c38585521
Slash <ew.kolesnikov1969@yandex.ru>
2023-01-04 02:05:26 +0500

    Добавил Ваню

--- settings.py
+++ settings.py
@@ -1,14 +1,17 @@
 from data import db_session
 from data.deliverymen import Deliverymen
 
-deliverymen = {'Миша': 835528178, 'Склад': 1}
+deliverymen = {'Миша': 835528178, 'Иван': 1960762355, 'Склад': 1}
 admin = 1904018585
 admin_2 = 635083191
-delyverymen_id = {'Миша': '@davannnnnn', 'Склад': '@sklad'}
+delyverymen_id = {'Миша': '@davannnnnn', 'Иван': '@n1nj4ch', 'Склад': '@sklad'}
 text_chat = '''Доставка осуществляется в нижеперечисленные районы города:
 
 Рынок-Звёздный-Стадион-Орлова : 
 @davannnnnn
+
+Швейка - Каравай - Стадион - Звёздный:
+@n1nj4ch
 
 Оплата производится только при получении товара!
 
