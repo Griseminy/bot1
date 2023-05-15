4a6fa961d6fffc50a9daf7c515724266e14c9bdd
Slash <ew.kolesnikov1969@yandex.ru>
2023-05-15 14:22:04 +0500

    тест бага

--- main.py
+++ main.py
@@ -1735,7 +1735,7 @@ def main():
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command | Filters.document | Filters.photo,
                                   handler, run_async=True))
     dp.add_handler(CallbackQueryHandler(forward_query_handler, run_async=True))
-    #dp.add_error_handler(error_handler)
+    dp.add_error_handler(error_handler)
     updater.start_polling()
     updater.idle()
 
